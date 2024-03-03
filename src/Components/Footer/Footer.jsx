f8a1ccb36374647d96486618ce539ce24ced2b82
lalaggv2 <luangu2020@gmail.com>
2024-03-03 08:55:47 -0500

    Cree componentes para items popular y footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import footer_logo from '../Assets/logo_big.png'
+import instagram_icon from '../Assets/instagram_icon.png'
+import pinterest_icon from '../Assets/pintester_icon.png'
+import whatsup_icon from '../Assets/whatsapp_icon.png'
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer-logo">
+            <img src={footer_logo} alt="" />
+            <p>L y F Confecciones</p>
+
+        </div>
+        <ul className="footer-links">
+            <li>Compania</li>
+            <li>Productos</li>
+            <li>Donde estamos</li>
+            <li>Acerca de</li>
+            <li>Contacto</li>
+        </ul>
+        <div className="footer-social-icons">
+            <div className="footer-icons-container">
+                <img src={instagram_icon} alt="" />
+            </div>
+            <div className="footer-icons-container">
+                <img src={pinterest_icon} alt="" />
+            </div>
+            <div className="footer-icons-container">
+                <img src={whatsup_icon} alt="" />
+            </div>
+        </div>
+        <div className="footer-copyright">
+            <hr />
+            <p>Copyright @ 2024 = All rights reserved</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
